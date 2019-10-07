ac0ab0dd2fa02aff2d5b6fc72aa7576b52ef2d35
Chris Collins <chriscollins238@gmail.com>
2019-10-07 20:51:19 +0100

    Use Nunjucks, remove multiple configs

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,51 @@
-var devConfig = require("./config/eleventy.dev.js")
-var prodConfig = require("./config/eleventy.prod.js")
+const htmlmin = require("html-minifier");
+const { DateTime } = require("luxon");
 
-// Load to correct .eleventy config file for the env
-module.exports = process.env.ELEVENTY_ENV === 'production' ? prodConfig : devConfig;
+module.exports = function (eleventyConfig) {
+    // Folders to copy to build dir (See. 1.1)
+    eleventyConfig.addPassthroughCopy("src/static");
+
+    if (process.env.ELEVENTY_ENV === 'production') {
+        // Minify HTML (including inlined CSS and JS) 
+        eleventyConfig.addTransform("compressHTML", function (content, outputPath) {
+            if (outputPath.endsWith(".html")) {
+                let minified = htmlmin.minify(content, {
+                    useShortDoctype: true,
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    minifyCSS: true,
+                    minifyJS: true
+                });
+                return minified;
+            }
+            return content;
+        });
+    }
+    
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+    });
+
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(new Date(dateObj), { zone: 'utc' }).toFormat("dd LLL yyyy");
+    });
+
+    eleventyConfig.addFilter("w3cDate", function (date) {
+        return date.toISOString();
+    });
+
+
+    return {
+        dir: {
+            input: "src/",
+            output: "dist",
+            includes: "_includes"
+        },
+        templateFormats: ["html", "md", "njk"],
+        htmlTemplateEngine: "njk",
+
+        // 1.1 Enable elventy to pass dirs specified above
+        passthroughFileCopy: true
+    };
+};
