d8fad3415ae36bcba5f65dac90c5547cafdd2faf
Chris Collins <chriscollins238@gmail.com>
2020-06-10 11:43:09 +0100

    Use JSON for data file

---
+++ src/_data/site.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "title": "Web Starter",
+  "description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PurgeCSS and PostCSS",
+  "url": "https://webstarter.chriscollins.me",
+  "author": "Chris Collins",
+  "meta_data": {
+    "twitter": "@scottishstoater",
+    "default_social_image": "/static/default_social_image.jpg"
+  },
+  "env": process.env.ELEVENTY_ENV === 'production'
+}
