1804c0cf01003768a2fb108b73a687510289f067
Chris Collins <chriscollins238@gmail.com>
2021-07-23 10:26:53 +0100

    Use Inter as default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,28 @@
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        'ws-blue': {
-          100: '#E6F0FF',
-          200: '#BFDAFF',
-          300: '#99C3FF',
-          400: '#4D97FE',
-          500: '#006AFE',
-          600: '#005FE5',
-          700: '#004098',
-          800: '#003072',
-          900: '#00204C',
-        },
-      }
-    },
-  },
-  variants: {},
-  plugins: [],
-  purge: process.env.NODE_ENV === 'production' ? {
-    enabled: true,
-    content: ['src/**/*.njk', 'src/**/*.js'],
-  } : {}
+	theme: {
+		fontFamily: {
+			'sans': ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'ws-blue': {
+					100: '#E6F0FF',
+					200: '#BFDAFF',
+					300: '#99C3FF',
+					400: '#4D97FE',
+					500: '#006AFE',
+					600: '#005FE5',
+					700: '#004098',
+					800: '#003072',
+					900: '#00204C',
+				},
+			}
+		},
+	},
+	variants: {},
+	plugins: [],
+	purge: process.env.NODE_ENV === 'production' ? {
+		enabled: true,
+		content: ['src/**/*.njk', 'src/**/*.js'],
+	} : {}
 }
