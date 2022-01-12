e85e537b039aef053009a44e7ba4f617fd638ca8
Chris Collins <chriscollins238@gmail.com>
2022-01-12 17:01:36 +0000

    Upgrade to Tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,10 +19,6 @@ module.exports = {
 			}
 		},
 	},
-	variants: {},
 	plugins: [],
-	purge: process.env.NODE_ENV === 'production' ? {
-		enabled: true,
-		content: ['src/**/*.njk', 'src/**/*.js'],
-	} : {}
+	content: ['src/**/*.njk', 'src/**/*.js']
 }
