9f836c7216f1aef32ce2f42729d27c56231fbb92
Chris Collins <chriscollins238@gmail.com>
2022-06-01 17:48:03 +0100

    Change wording

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
 	"title": "Eleventy Web Starter",
-	"description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PurgeCSS and PostCSS",
+	"description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PostCSS and esbuild",
 	"url": "https://eleventywebstarter.netlify.app",
 	"author": "Chris Collins",
 	"meta_data": {
