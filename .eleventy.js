04807cd1e14b69e064732785afc28cf4ca1c630a
Chris Collins <chriscollins238@gmail.com>
2022-09-28 19:45:08 +0100

    Simplify the configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,58 @@
-const filters = require('./utils/filters.js')
-const transforms = require('./utils/transforms.js')
-const collections = require('./utils/collections.js')
+const htmlmin = require("html-minifier");
+const isProduction = process.env.ELEVENTY_ENV === 'production';
+const { DateTime } = require('luxon');
 
 module.exports = function (eleventyConfig) {
 	// Folders to copy to build dir (See. 1.1)
 	eleventyConfig.addPassthroughCopy("src/static");
 
-	// Filters 
-	Object.keys(filters).forEach((filterName) => {
-		eleventyConfig.addFilter(filterName, filters[filterName])
-	})
+	/* ---------------------------------------------
+        Filters
 
-	// Transforms
-	Object.keys(transforms).forEach((transformName) => {
-		eleventyConfig.addTransform(transformName, transforms[transformName])
-	})
+        A filter is a function which can be used 
+        within templating syntax to transform data 
+        into a more presentable format.
+	 -------------------------------------------- */
 
-	// Collections
-	Object.keys(collections).forEach((collectionName) => {
-		eleventyConfig.addCollection(collectionName, collections[collectionName])
-	})
+	// htmlDateString: Return the date in the format e.g. 2022-12-25
+	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+		return DateTime.fromJSDate(dateObj, {
+			zone: 'utc'
+		}).toFormat('yyyy-LL-dd');
+	});
+
+	// postDate: Return the date in a human readable format e.g. Nov 23, 2020
+	eleventyConfig.addFilter("postDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+	});
+
+	/* ---------------------------------------------
+        Collections
+        
+        A collection allows you to group content
+        to loop through within 11ty.
+	 -------------------------------------------- */
+
+    
+
+	/* ---------------------------------------------
+        Transforms
+
+        These can modify a template's output.
+	 -------------------------------------------- */
+
+	// Minify HTML in Production
+	eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+		if (outputPath && outputPath.endsWith(".html") && isProduction) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true
+			});
+			return minified;
+		}
+		return content;
+	});
 
 	// This allows Eleventy to watch for file changes during local development.
 	eleventyConfig.setUseGitIgnore(false);
