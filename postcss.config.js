3bddcb26d6da0889d04a55200f4fa15817a780ce
Chris Collins <chriscollins238@gmail.com>
2022-10-03 22:42:41 +0100

    Include tailwindcss/nesting

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,8 @@ const isProduction = process.env.ELEVENTY_ENV === 'production';
 module.exports = {
 	plugins: [
 		require('postcss-import'),
-		require('tailwindcss'),
+        require('tailwindcss/nesting'),
+        require('tailwindcss'),
 		require('autoprefixer'),
         isProduction && require('cssnano')
 	]
