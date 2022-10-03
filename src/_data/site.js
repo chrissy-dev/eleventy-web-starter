6697a84bd744c9e31fa85494f5c867a95a3b29be
Chris Collins <chriscollins238@gmail.com>
2022-10-03 22:42:30 +0100

    site.env should return ELEVENTY_ENV

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,11 @@
 module.exports = {
 	"title": "Eleventy Web Starter",
-	"description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PostCSS and esbuild",
+	"description": "Eleventy Web Starter is a boilerplate for projects built with Eleventy, Tailwind, PostCSS and esbuild",
 	"url": "https://eleventywebstarter.netlify.app",
 	"author": "Chris Collins",
 	"meta_data": {
 		"twitter": "@chrissy_dev",
 		"opengraph_default": "/static/opengraph-default.jpg"
 	},
-	"env": process.env.ELEVENTY_ENV === 'production'
+	"env": process.env.ELEVENTY_ENV
 }
