9e35eabb27e1e7f88e5c678b36401ef68379a44c
Chris Collins <chriscollins238@gmail.com>
2022-10-03 23:42:09 +0100

    Add a basic site header

--- src/_assets/scripts/scripts.js
+++ src/_assets/scripts/scripts.js
@@ -1,1 +1,13 @@
-console.log(`I was loaded at ${Date(Date.now()).toString()}`);
+(async () => {
+	const menuToggle = document.querySelector('#mobile-menu-toggle');
+	const mobileMenu = document.querySelector('#mobile-menu');
+
+    menuToggle.addEventListener('click', (e) => {
+        // If the current value is 'false' but we've just clicked it - it's going to be expanded.
+        let isExpanded = menuToggle.getAttribute("aria-expanded") == 'false';
+        menuToggle.setAttribute("aria-expanded", isExpanded);
+        mobileMenu.setAttribute("aria-expanded", isExpanded);
+        mobileMenu.classList.toggle('hidden');
+    });
+})();
+
